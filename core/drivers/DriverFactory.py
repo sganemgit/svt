b7ce11a124ec140836268d4bff93646d9faf3b72
SV_shared_team <svshared@ladh444.iil.intel.com>
2020-06-14 18:34:03 +0300

    fixed typo

--- core/drivers/DriverFactory.py
+++ core/drivers/DriverFactory.py
@@ -2,14 +2,14 @@ class DriverFactory(object):
     '''This class is responsible for creation of drivers'''
 
     @classmethod
-    def create_driver(cls, driver_type, port_info):
+    def create_driver(cls, driver_type, device_info):
         ''' 
         Class method that creates driver according to driver type.
         If driver type not supported RuntimeError will be raised.
        ''' 
         if driver_type == 'sv':
             from core.drivers.svdriver import SvDriver
-            return SvDriver(port_info)  
+            return SvDriver(device_info)  
 
         error = F"Failed to create driver of type {driver_type}, Unsupported dirver type"
         raise RuntimeError(error)
