a1970fa1f4baf6a8fbbb9f1eb9f688d289f8852e
Shady Ganem <shady.ganem@intel.com>
2020-08-15 21:34:13 +0300

    added an LmServer module to handle comuniction with the LM Server running on the IMC

---
+++ devices/mev/LmServer.py
@@ -0,0 +1,40 @@
+
+# @author Shady Ganem <shady.ganem@intel.com>
+
+
+
+from socket import *
+import time
+
+
+
+class LmServer:
+    '''
+        This class handles connections to the LM Server that runs on the MEV IMC
+    '''
+
+
+    def __init__(self, ip = '127.0.0.1', port = '5077'):
+        self._socket = socket()
+        self._socket.settimeout(10)
+        self._IP = ip 
+        self._PORT = port
+    
+    def set_imc_tcp_ip_address(self, ip, port):
+        self._IP = ip
+        self._PORT = port
+    
+    def get_imc_tcp_ip_address(self, ip, port):
+        returnt self.IP, self.PORT 
+
+    def connect(self):
+        try:
+            self._socket(self.get_imc_tcp_ip_address())
+            time.sleep(0.2)
+        except Exception as e:
+            print('could not connect to imc')
+            raise e
+
+    def lm_server_recv(self):
+        pass
+
