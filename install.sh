eb8bbd420f3f6885b89167248f35dd51b17a0c68
SV_shared_team <svshared@ladh2368.iil.intel.com>
2021-03-24 18:52:49 +0200

    install.sh will now install python dependencies and will set the env vars

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo 'export PYTHONPATH=$PYTHONPATH:'$PWD >> ~/.bashrc
-source ~/.bashrc
+sudo pip install -r $PWD/core/depend/requirements.txt
+
+if [[ -z PYTHONPATH ]]; then
+	EXPORT_CMD='export PYTHONPATH='$PWD
+	echo ${EXPORT_CMD} >> ~/.bashrc
+	source ~/.bashrc
+else 
+	export PYTHONPATH=PYTHONPATH:$PWD
+	EXPORT_CMD='export PYTHONPATH=$PYTHONPATH:'$PWD
+	if [[ $PYTHONPATH == *'$PWD'* ]]; then
+		echo -e "PYTHONPATH OK"
+	else
+		echo ${EXPORT_CMD} >> ~/.bashrc
+		source ~/.bashrc
+	fi
+fi
 echo -e "Done"
