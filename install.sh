04b7c860477c475635df7db89f25e2c470073ea4
Shady Ganem <shady.ganem@intel.com>
2021-03-31 23:33:03 +0300

    the install script is capable of installing openipc for linux

--- install.sh
+++ install.sh
@@ -1,46 +1,66 @@
 #!/bin/bash
 
 # @author Shady Ganem <shady.ganem@intel.com>
-
+set -e
 echo -e "Input args: $@"
-echo -e "\nInstalling Depedencies\n"
-sudo yum -y install python3 python2
-python3 -m ensurepip
-yum -y install python-pip
-pip3 install -r $PWD/core/depend/requirements.txt
-pip install -r $PWD/core/depend/requirements.txt
 
-
-if [[ -n "$1" ]]
+if [[ "$1" -eq "intec" ]]
 then 
-	echo -e "in if"
-	if [[ "$1" -eq "pythonsv" ]]
-	then 
-		if [[ "$2" -eq "mev" ]]
-		then
-			sudo python3 -m pip install mtevans --upgrade -i https://ubit-artifactory-or.intel.com/artifactory/api/pypi/DSD-SD-SV-Tools-pypi-local/simple --user
-			sudo python3 -m mtevans.update_tools
-			sudo python3 -m mtevans.getdefs
-		fi
-	elif [[ "$1" -eq "intec" ]]
-	then 
-		sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/30/winehq.repo	
-		sudo dnf -y install winehq-stable
+	#sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/30/winehq.repo	
+	#sudo dnf -y install winehq-stable
+	echo -e ""
+fi
+
+if [[ "$1" == "openipc" ]]
+then
+	if [[ ! -d ~/bin/openIPC ]]
+	then
+		echo -e "Making Directory $HOME/bin/openIPC"
+		mkdir -p ~/bin/openIPC
+		
+		read -p "Username: " USERNAME
+		read -s -p "Password: " PASSWORD
+		ARTIFACT=OpenIPCDebugLibrary.StandaloneLinux.1.2106.5146.600.x64.tar.gz
+		curl -u $USERNAME:$PASSWORD https://ubit-artifactory-or.intel.com/artifactory/list/dsdjf-pvt-repo/OpenIPC/2109/Linux/OpenIPCDebugLibrary.StandaloneLinux.1.2106.5146.600.x64.tar.gz -o $HOME/bin/$ARTIFACT
+		tar -C $HOME/bin/openIPC -xf $HOME/bin/$ARTIFACT 
+		rm -rf $HOME/bin/$ARTIFACT
+	else
+		echo -e "openIPC already installed at $HOME/bin/openIPC"
 	fi
 fi
 
-if [[ -z PYTHONPATH ]]
+if [[ "$1" == "pythonsv" ]]
+then 
+	if [[ "$2" -eq "mev" ]]
+	then
+		python3 -m pip install mtevans --upgrade -i https://ubit-artifactory-or.intel.com/artifactory/api/pypi/DSD-SD-SV-Tools-pypi-local/simple --user
+		python3 -m mtevans.update_tools
+		python3 -m mtevans.getdefs
+		rm -rf update_tools_logs namednodes.log
+	fi
+fi
+
+if [[ "$1" == "" ]]
 then
-	EXPORT_CMD='export PYTHONPATH='$PWD
-	echo ${EXPORT_CMD} >> ~/.bashrc
-	. ~/.bashrc
-else 
-	if [[ $PYTHONPATH == *'svt'* ]]; then
-		echo -e "PYTHONPATH OK"
-	else
-		EXPORT_CMD='export PYTHONPATH=$PYTHONPATH:'$PWD
+	echo -e "\nInstalling Depedencies\n"
+	sudo yum -y install python3 python2
+	python3 -m ensurepip
+	sudo yum -y install python-pip
+	pip3 install -r $PWD/core/depend/requirements.txt
+	pip install -r $PWD/core/depend/requirements.txt
+	if [[ -z PYTHONPATH ]]
+	then
+		EXPORT_CMD='export PYTHONPATH='$PWD
 		echo ${EXPORT_CMD} >> ~/.bashrc
 		. ~/.bashrc
+	else 
+		if [[ $PYTHONPATH == *'svt'* ]]; then
+			echo -e "PYTHONPATH OK"
+		else
+			EXPORT_CMD='export PYTHONPATH=$PYTHONPATH:'$PWD
+			echo ${EXPORT_CMD} >> ~/.bashrc
+			. ~/.bashrc
+		fi
 	fi
 fi
 echo -e "\nDone\n"
