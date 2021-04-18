c519db1fa2694c7c5a6beaa4d534322c9535880f
Shady Ganem <shady.ganem@intel.com>
2021-04-18 18:21:50 +0300

    MEV theraml management test development - saving progress

--- tests/ThermalManagement/generic/TemperatureSweepTest.py
+++ tests/ThermalManagement/generic/TemperatureSweepTest.py
@@ -6,6 +6,20 @@ TEST = True
 from tests.ThermalManagement.generic.ThermalManagementBase import ThermalManagementBase
 
 class TemperatureSweepTest(ThermalManagementBase):
+    
+    def execute_iteration(self):
+        pass
 
     def run(self):
         self.log.info("Temperature Sweep Test")
+        self.print_input_args()
+        self.init_test_args()
+        
+        for iteration in range(self.num_of_iterations):
+            self.test_iteration = iteration
+            try:
+                self.execute_iteration()
+            except Exception as e:
+                self.append_fail_reason(str(e))
+            finally:
+                self.summarize_iteration()
--- tests/ThermalManagement/generic/ThermalManagementBase.py
+++ tests/ThermalManagement/generic/ThermalManagementBase.py
@@ -3,7 +3,27 @@ from core.tests.testBase import testBase
 
 
 class ThermalManagementBase(testBase):
-    pass
+    
+    def print_input_args(self):
+        self.log.info("-"*80)
+        self.log.info("input arguments", 'o')
+        for key, val in self.args.items():
+            self.log.info(f"{key} : {val}")
+        self.log.info("-"*80)
+
+    def init_test_args(self):
+        self.ftdi_index = int(self.args.get("ftdi_index", "1"))
+        self.num_of_iterations = int(self.args.get("num_of_iter", "1"))
+
+    def summarize_iteration(self):
+        self.log.info("-"*80)
+        #TODO: need to implement the iteration summary function
+        self.log.info("Iteration summary goes here")
+        self.log.info("-"*80)
+
+    def set_T_case(self, intec, temparture_value):
+        intec.SetTemperature(temaprute_value)
+
 
 
         
