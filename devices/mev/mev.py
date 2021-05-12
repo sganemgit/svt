2951c6f3d1b0c7f1bb94b8c6e674111cfae2578a
Shady Ganem <shady.ganem@intel.com>
2021-05-12 15:34:49 +0300

    [MEV] added mehtod to query the acc clk status

--- devices/mev/mev.py
+++ devices/mev/mev.py
@@ -63,9 +63,63 @@ class mev(mevBase):
             return self.data.mev_default_thermtrip_b_threshold
         elif hysteresis_dircetion == "down":
             return self.data.mev_default_thermtrip_b_threshold_hysteresis
-    
-    def get_acc_ss_clock_status(self):
-        if self.driver is not None:
-            pass
 
+    def get_pvt_vid_vcc_pm(self):
+        #TODO return fuse value via sv driver
+        pass
+
+    def get_pvt_vid_vcc_sm(self):
+        #TODO return fuse value via sv driver
+        pass
+
+    def get_pvt_vid_vnn(self):
+        #TODO return fuse value via sv driver
+        pass
+
+    def get_pvt_vnn_itd_disable(self):
+        #TODO return fuse value via sv driver
+        pass
         
+    def get_pvt_vcc_itd_disable(self):
+        #TODO return fuse value via sv driver
+        pass
+    
+    def get_pvt_ts_cattrip_disable(self):
+        #TODO return fuse value via sv driver
+        pass
+    
+    def get_pvt_use_uncalibrated_ts(self):
+        #TODO return fuse value via sv driver
+        pass
+    
+    def get_PVT_TS_CATTRIP_6_0(self):
+        #TODO return fuse value via sv driver
+        pass
+    
+    def get_all_tm_fuses(self):
+        #TODO finish this method by calling the other mehtods
+        fuse_dict = dict()
+        return fuse_dict
+ 
+    def get_acc_ss_cpu_clk_status(self):
+        if self.driver is not None:
+            core_pll_cfg_dict = dict()
+            cfg_1 = self.driver.read_csr(self.data.mev_cpu_cfg_pll_1_inst)
+            cfg_2 = self.driver.read_csr(self.data.mev_cpu_cfg_pll_2_inst)
+
+            #cores 0,1,2,3
+            postdiv_2a = (cfg_1 >>  16) & 0x7
+            postdiv_2b = (cfg_1 >>  19) & 0x7
+            #cores 6,7,8,9
+            postdiv_3a = (cfg_1 >>  24) & 0x7
+            postdiv_3b = (cfg_1 >>  27) & 0x7
+            #core 10,11,12,13
+            postdiv_4a = cfg_2 & 0x7
+            postdiv_4b = (cfg_2 >> 3) & 0x7
+            #cores 3,4,14,15
+            postdiv_5a = (cfg_2 >> 8) & 0x7 
+            postdiv_5b = (cfg_2 >> 11) & 0x7 
+            
+            #TODO finish this method. should return a dict with the each pll clk
+
+
--- devices/mev/mevData.py
+++ devices/mev/mevData.py
@@ -35,4 +35,14 @@ class mevData(commonData):
     mev_pvt_config_file_path = "/etc/hwconf/active/pvt"
 
     mev_pvt_log_file_path = "/var/dts_log.txt"
+    
+    mev_cpu_cfg_pll_0_inst = 0x00600000 + 0x00000000
+    mev_cpu_cfg_pll_1_inst = 0x00600000 + 0x00000004 # cores 0,1,2,3,6,7,8,9
+    mev_cpu_cfg_pll_2_inst = 0x00600000 + 0x00000008 # cores 10,11,12,13,4,5,14,15 
+    mev_cpu_cfg_pll_3_inst = 0x00600000 + 0x0000000c 
+    mev_cpu_cfg_pll_4_inst = 0x00600000 + 0x00000010
+    mev_cpu_cfg_pll_5_inst = 0x00600000 + 0x00000018
+    mev_cpu_cfg_pll_6_inst = 0x00600000 + 0x00000018 
+    mev_remap_cfg_7_inst = 0x00600000 + 0x0000001c
+    mev_pll_ro_0_inst = 0x00600000 + 0x00000020
 
